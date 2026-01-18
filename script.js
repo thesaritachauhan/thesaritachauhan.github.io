63365f32d72e402a1517ee0861e9f14951b9e55f
LAKSHYA SINGH CHAUHAN <85747258+lakshyaelite@users.noreply.github.com>
2026-01-18 11:13:01 +0530

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    loader.style.opacity = '0';
+    setTimeout(() => loader.style.display = 'none', 500);
+  });
